d55a5f6fcdd8aaca6f5f7cfaafaac6bb3352ae34
NoPo6 <gboguslavskiy03@gmail.com>
2026-04-27 16:31:58 +0300

    models added, changed venv, first migration has been done

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,102 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+# Таблица с профилями
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    nickname = models.CharField(max_length=50, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
+    def __str__(self):
+        return self.user.username
+
+# Таблица с тэгами 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+# Кастомный менеджер, в котором определяем типичные выборки (“лучшие” и “новые” вопросы).
+class QuestionManager(models.Manager):
+    def get_new(self):
+        return self.order_by('-created_at')
+
+    def get_best(self):
+        return self.annotate(likes_count=models.Count('questionlike')).order_by('-likes_count')
+
+# Таблица вопросов
+class Question(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст вопроса')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questions')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    tags = models.ManyToManyField(Tag, related_name='questions', through='QuestionTag')
+
+    objects = QuestionManager()
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('question_detail', args=[self.pk])
+
+# Таблица ответов 
+class Answer(models.Model):
+    text = models.TextField(verbose_name='Текст ответа')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answers')
+    is_correct = models.BooleanField(default=False, verbose_name='Правильный ответ')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'Ответ на "{self.question.title}"'
+
+# Таблица для связи юзера с его лайков вопроса
+class QuestionLike(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='questionlike')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Лайк вопроса'
+        verbose_name_plural = 'Лайки вопросов'
+        unique_together = ('question', 'user')   
+
+
+# Таблица для связи юзера с его лайком ответа
+class AnswerLike(models.Model):
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name='answerlike')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Лайк ответа'
+        verbose_name_plural = 'Лайки ответов'
+        unique_together = ('answer', 'user')
+
+
+# Промежуточная таблица для ManyToMany
+class QuestionTag(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('question', 'tag')
